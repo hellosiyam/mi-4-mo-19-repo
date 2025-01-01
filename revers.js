c38f994af1a7c4d43634c77484e54dd5808c5429
hellosiyam <hellosiyambd@gmail.com>
2025-01-01 19:41:19 +0600

    complite revers

---
+++ revers.js
@@ -0,0 +1,124 @@
+// const sentence = "I Am Learning Web Dev."
+// for(const latter of sentence){
+//     console.log(latter);
+    
+// }
+
+// const sentence = 'I Am Learning Web Dev.'
+// for(const latter of sentence){
+//     console.log(latter);
+    
+// }
+
+// const sentence = 'I Am Learning Web Dev.'
+// let revers=' '
+// for(const latter of sentence){
+//     revers=latter+revers;
+// }
+// console.log(revers);
+
+// const sentence='I Am a Good Student'
+// let revers=' '
+// for(const latter of sentence){
+//     revers=latter+revers;
+// }
+// console.log(revers);
+
+// const sentence ='I Am Learning Web Devlopment'
+// let revers=' '
+// for(const latter of sentence){
+//    revers=latter+sentence 
+// }
+// console.log(revers);
+
+// const sentence = 'I am Leraning web Devlopment'
+// let rev=' '
+// for(let i =0; i<sentence.length; i++){
+//     const latter=sentence[i];
+//     rev=latter+rev
+// }
+// console.log(rev);
+
+
+// const sentence ='I Am Learning Web Devlopment'
+// const revers= sentence.split('').reverse(' ').join('')
+// console.log(revers);
+
+// const siyam='I am leraning wev dev'
+// let fev=''
+// for(const sym of siyam){
+//     fev=sym+fev
+//     console.log(fev);   
+// }
+
+// const siyam='I Am a Web Devlpoer'
+// let rev=''
+// for(i=0; i<siyam.length; i++){
+//     rev=siyam[i]+rev
+// }
+// console.log(rev);
+
+// const siyam='I am a web devloper'
+// let rev=""
+// for(const sym of siyam){
+//     rev=sym+rev
+// }
+// console.log(rev);
+
+// const siyam= 'I am lerning web devloping'
+// let rev=''
+// for(i=0;i<siyam.length; i++){
+//     rev=siyam[i]+rev;
+// }
+// console.log(rev);
+
+// const siyam='I am a good student'
+// let rev=''
+// for(const sym of siyam){
+//     rev=sym+rev;
+// }
+// console.log(rev);
+
+// const siyam = 'I am doing well'
+// let rev = ''
+// for(i=0; i<siyam.length; i++){
+//     rev=siyam[i]+rev;
+// }
+// console.log(rev);
+
+// const sentence= 'I am a good student'
+// const revers = sentence.split('').reverse('').join('')
+// console.log(revers);
+
+// const siyam= 'I am doing well'
+// const sym=siyam.split('').reverse('').join('')
+// console.log(sym);
+
+// const siyam = 'I am good student'
+// const sym=siyam.split('').reverse('').join('')
+// console.log(sym);
+const siyam = 'I am a good student'
+let rev=''
+// for(i=0; i<siyam.length; i++){
+//     rev=siyam[i]+rev
+// }
+// console.log(rev);
+// for(const sym of siyam){
+//     rev=sym+rev
+// }
+// console.log(rev);
+// const sym=siyam.split('').reverse('').join('')
+// console.log(sym);
+
+for(i=0; i<siyam.length; i++){
+    rev=siyam[i]+rev;
+}
+console.log(rev);
+
+
+
+
+
+
+
+

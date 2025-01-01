1f4077b7b87c6121b7451e50cfe18efe5362f15d
hellosiyam <hellosiyambd@gmail.com>
2025-01-01 16:57:42 +0600

    Complite case.js

---
+++ case.js
@@ -0,0 +1,22 @@
+const subject="Chemistry"
+const book="chemistry"
+
+if (subject.toLocaleLowerCase() === book.toLocaleLowerCase()) {
+    // console.log("I am doing well my exam"); 
+}
+
+else{
+    // console.log("Mara Khawa sara");
+    
+}
+
+let or=" white "
+let roll="white"
+if(or.trim === roll.trim ) {
+    console.log("I read in class VI");
+    
+}
+else{
+    console.log("This is note valide");
+    
+}

fc50bc591ecbe99d7cd55a91ec845c7e4b3fddba
hellosiyam <hellosiyambd@gmail.com>
2025-01-01 21:58:28 +0600

    complite loops

---
+++ loop.js
@@ -0,0 +1,79 @@
+const mobile={
+    name:'Apple',
+    color: 'Red',
+    price: '50000',
+    isWaterproof: true,
+    camera: '16mp'
+}
+// for(const key in mobile){
+//     console.log(key);
+//     console.log(mobile[key]);
+    
+    
+// }
+
+const key=Object.keys(mobile);
+console.log(key);
+for(const keys of key){
+    console.log(keys,':', mobile[keys]);
+    
+}
+
+
+// for(const key in mobile){
+//     console.log(key);
+//     console.log(mobile[key]);
+    
+// }
+
+// const key=Object.keys(mobile);
+// console.log(key);
+// for(const keys of key){
+//     console.log(keys,':', mobile[keys]);
+    
+// }
+
+
+// for(const key in mobile){
+//     console.log(key);
+//     console.log(mobile[key]);
+    
+    
+// }
+
+// const key = Object.keys(mobile);
+// console.log(key);
+// for(const keys of key){
+//     console.log(keys,':', mobile[keys]);
+    
+// }
+
+
+// for(const key in mobile){
+//     console.log(key);
+//     console.log(mobile[key]);
+    
+    
+// }
+
+// const key=Object.keys(mobile);
+// console.log(key);
+// for(const keys of key){
+//     console.log(keys,':',mobile[keys]);
+    
+// }
+
+    
+
+// for(const key in mobile){
+//     // console.log(key);
+//     // console.log(mobile[key]);
+    
+// }
+
+// const key=Object.keys(mobile);
+// console.log(key);
+// for(const keys of key){
+//     console.log(keys,':', mobile[keys]);
+    
+// }

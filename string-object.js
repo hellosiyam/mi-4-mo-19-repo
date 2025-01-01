95fb4721a5a2b0bc697246cc8ea6abd13a63e6e6
hellosiyam <hellosiyambd@gmail.com>
2025-01-01 16:39:12 +0600

    Complite string object

--- string-object.js
+++ string-object.js
@@ -1,2 +1,13 @@
 const country="Bangladesh"
-console.log(country);
+let zila='Dhaka'
+var school=`High school`
+const thana=new String("school")
+
+// console.log( zila.length);
+
+const num=[54, 5, 85, 14, 23, 98, 25, 21, 71]
+num[5]= 600
+console.log(num)
+
+
+
